69807f4bda915c3d6de96741606838c4da4095d0
DinoBektesevic <dinob@uw.edu>
2021-04-08 21:10:24 -0700

    Fix path to authentication script.

--- autobuilder/auth/authenticate.sh
+++ autobuilder/auth/authenticate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Script invokes credentials setup and exports them to the environment
-python3 setUpCredentials.py
+python3 ~/.install/RubinAWS/autobuilder/auth/setUpCredentials.py
 
 export AWS_ACCESS_KEY_ID=`cat ~/.condor/publicKeyFile`
 export AWS_SECRET_ACCESS_KEY=`cat ~/.condor/privateKeyFile`
